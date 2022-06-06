8f4a83aad9aa39203cbee1015bb799c2d54c9f2d
Dario Baldi <baldi27@gmail.com>
2022-06-06 23:30:48 +0200

    Set up Express server

--- ImageProcessingAPI/src/index.ts
+++ ImageProcessingAPI/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+import express from 'express';
+const app = express()
+const port = 5050
+
+app.get('/', (_req: express.Request, res: express.Response) => {
+  res.status(200).send('The server is working')
+})
+
+app.listen(port, () => {
+  console.log(`ImageProcessingAPI app listening in port ${port}`)
+})
