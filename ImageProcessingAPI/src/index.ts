5b80b636a40539584832e97ddacf109db18672b5
Dario Baldi <baldi27@gmail.com>
2022-06-06 23:45:32 +0200

    Delete jasmine.json fantome file

--- ImageProcessingAPI/src/index.ts
+++ ImageProcessingAPI/src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-const app = express()
-const port = 5050
+const app = express();
+const port = 5050;
 
 app.get('/', (_req: express.Request, res: express.Response) => {
-  res.status(200).send('The server is working')
-})
+  res.status(200).send('The server is working');
+});
 
 app.listen(port, () => {
-  console.log(`ImageProcessingAPI app listening in port ${port}`)
-})
+  console.log(`ImageProcessingAPI app listening in port ${port}`);
+});
